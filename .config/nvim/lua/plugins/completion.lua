7e768ad54672a25a9264dba9d86bc4ae92572598
ScratchBestProgrammingLanguage <claclagamer.pratesi@gmail.com>
2025-07-28 19:57:55 +0200

    test

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,13 +1,14 @@
 return {
-	'saghen/blink.cmp',
-	version = "1.*",
-  	dependencies = { 'rafamadriz/friendly-snippets' },
-	opts = {
-		keymap = {
-			['<Tab>'] = {'accept', 'fallback'},
-			['<C-n>'] = {'select_next', 'fallback'},
-			['<C-b>'] = {'select_prev', 'fallback'},
-		}
-	},
-	config = true,
+    'saghen/blink.cmp',
+    version = "1.*",
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    opts = {
+        keymap = {
+            ['<Tab>'] = { 'accept', 'fallback' },
+            ['<C-n>'] = { 'select_next', 'fallback' },
+            ['<C-b>'] = { 'select_prev', 'fallback' },
+        }
+    },
+    lazy = true,
+    event = "InsertEnter",
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,16 @@
+local keymaps = {
+    { "<leader>lh", vim.lsp.buf.hover,          desc = "[L]sp [H]over" },
+
+    -- Gotos
+    { "<leader>gd", vim.lsp.buf.definition,     desc = "[G]oto [D]efinition" },
+    { "<leader>gi", vim.lsp.buf.implementation, desc = "[G]oto [I]mplementation" },
+
+    -- Code
+    { "<leader>ca", vim.lsp.buf.code_action,    desc = "[C]ode [A]actions" },
+    { "<leader>lr", vim.lsp.buf.rename,         desc = "[L]sp [R]ename" },
+}
+
+-- Zls specific
 vim.lsp.enable("zls")
 
 return {
@@ -7,4 +20,5 @@ return {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
     },
+    key = keymaps,
 }
